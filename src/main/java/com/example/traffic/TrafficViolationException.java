c7787f3a98632a334f0d7e948337ddda47be4b6c
AQSHAFATHIMA <aqshafathima.i2024@vitstudent.ac.in>
2026-09-10 12:33:23 +0530

    Add TrafficViolationException class

---
+++ src/main/java/com/example/traffic/TrafficViolationException.java
@@ -0,0 +1,11 @@
+package com.example.traffic;
+
+public class TrafficViolationException
+        extends Exception {
+
+    public TrafficViolationException(
+            String message) {
+
+        super(message);
+    }
+}
